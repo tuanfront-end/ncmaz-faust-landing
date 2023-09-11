946f913cc59d9dda6020530ea66138d8e78959b5
Le Anh Tuan <leanhtuan@Les-MacBook-Pro.local>
2023-09-11 19:58:25 +0700

    update buylink;

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,6 @@
 export const DEMO_PAGE_LINK = 'https://ncmaz-faust-delta.vercel.app'
-export const BUY_LINK = 'https://themeforest.net/user/chisnghiax/portfolio'
+export const BUY_LINK =
+  'https://themeforest.net/item/ncmaz-nextjs-headless-wordpress-blog-magazine/47815656'
 export const PRICE = '$44'
 export const SUPPORT_LINK = 'https://help.chisnghiax.com'
 export const DOCS_LINK =
